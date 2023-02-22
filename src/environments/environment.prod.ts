a2a98eaa495fb452891cce7e7d6e3b2203049160
JMVillota <jeffersonvillota@gmail.com>
2023-02-22 03:58:39 -0500

    Cambios Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // base_url: 'https://backend-condominios-production.up.railway.app/'
-  base_url: 'http://localhost:3001/'
+  base_url: 'https://backend-condominios-production.up.railway.app/'
+  // base_url: 'http://localhost:3001/'
 };
